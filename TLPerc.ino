4df0a3df782b90f967e004cc62c9da3f1511cfb9
TSUJI Taktuto <zerocrossroad@gmail.com>
2014-10-11 04:39:52 +0900

    Create TLPerc.ino

---
+++ TLPerc.ino
@@ -0,0 +1,38 @@
+//TLPerc.ino
+
+#include <SoftwareSerial.h>
+
+#define BT_RX 11
+#define BT_TX 10
+
+SoftwareSerial btSerial(BT_RX, BT_TX);
+
+int i;
+int signal;
+
+void setup(){
+	for(i = 0;i < 8;i++){
+		pinMode(i+2,INPUT);
+	}
+	signal = 0;
+	Serial.begin(115200);
+	btSerial.begin(115200);
+}
+void loop(){
+	for(i = 0;i < 8;i++){
+		if(digitalRead(i+2) == LOW){
+			temp = pow(2,i);
+			if(temp > 2){
+				temp++;
+			}
+			signal += temp;
+		}
+	}
+	if(signal < 16){
+		Serial.write("0");
+		btSerial.write("0");
+	}
+	Sirial.println("%x",signal);
+	btSirial.println("%x",signal);
+	signal = 0;
+}
